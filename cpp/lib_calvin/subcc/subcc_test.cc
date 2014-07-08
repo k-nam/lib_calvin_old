42a85ab790b3947d438d32117243fe4f3225c5a1
Calvin Caulfield <calvin.caulfield@gmail.com>
2014-07-08 17:09:50 +0900

    Changed windows native file processing to c++ wrapper classes

--- cpp/lib_calvin/subcc/subcc_test.cc
+++ cpp/lib_calvin/subcc/subcc_test.cc
@@ -3,46 +3,34 @@
 #include "fcntl.h" // for open, read
 //#include "unistd.h" // for constants
 #include <Windows.h>
-
+#include <fstream>
+#include <streambuf>
 #include "compiler.h"
    
 
 void subcc::subccTest(int argc, char *argv[]) {
 	using namespace subcc;
-	using lib_calvin::string;
+	using std::string;
 	using std::cout;
-	// 10MB will be enough for a source file
-	int const MAX_FILE_SIZE = 10000000;
-  //cout << "size of subcc parser is " << sizeof (Parser) << endl;
-  //cout << "size of subcc lexer is " << sizeof (SubccLexer) << endl;
-	std::cout << "\n---------- Beginning subcc test -----------\n";
-  char *buffer = new char[MAX_FILE_SIZE]; // store entire file 
-  int fileSize = 0;
-	char *inputFileName = NULL;
+	char *inputFileName = "../subcc_run/test/a1.c";
+	string sourceText;
   if (argc != 2) {
-		inputFileName = "../subcc_run/test/a1.c";
     cout << "usage: subcc <input-file>, will use default value:" << "\"" << inputFileName << "\"\n";
-
 	} else {
 		inputFileName = argv[1];
 	}
-	HANDLE inputFile = CreateFile((LPCTSTR)
-		inputFileName, GENERIC_READ, 0, NULL, OPEN_EXISTING, 
-											FILE_ATTRIBUTE_NORMAL, NULL);
-	if (inputFile == INVALID_HANDLE_VALUE) {
-		cout << "open file fail: " << inputFileName << "\n";
-		exit(0);
-	}
-	DWORD numBytesRead = 0;
-	BOOL read = ReadFile(inputFile, buffer, MAX_FILE_SIZE, &numBytesRead, NULL);
-	cout << "reading size: " << numBytesRead << "\n";
 
-  string sourceFile(buffer, numBytesRead); // entire input
-	cout << "input size is: " << sourceFile.size() << "\n";
+	std::ifstream t(inputFileName);
+	t.seekg(0, std::ios::end);   
+	sourceText.reserve(t.tellg());
+	t.seekg(0, std::ios::beg);
+	sourceText.assign((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+
+	cout << "input size is: " << sourceText.size() << "\n";
   string header("void printStr( char s[80] ); \n void printInt ( int a ); \n\
       void printNewLine(); \n");
-  string input = header + sourceFile;
+  string inputToCompiler = header + sourceText;
 	//while(true) {
-		compile(input);
+		compile(inputToCompiler);
 	//}
 }
