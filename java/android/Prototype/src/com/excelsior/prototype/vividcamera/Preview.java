959f886b20672a118e77c7714935d33f20192603
Calvin Caulfield <calvin.caulfield@gmail.com>
2014-07-18 14:14:17 +0900

    continue

---
+++ java/android/Prototype/src/com/excelsior/prototype/vividcamera/Preview.java
@@ -0,0 +1,13 @@
+package com.excelsior.prototype.vividcamera;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.excelsior.prototype.R;
+
+public class Preview extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.vivid_camera__preview);
+	}
+}
