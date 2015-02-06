2b35c0e12c37fd423037b5e9a41fcfa056da6643
Calvin Caulfield <calvin.caulfield@gmail.com>
2015-02-06 12:49:50 +0900

    Starting autocomplete AJAX for English words.

--- java/web/web_01/src/dictionary/Dictionary.java
+++ java/web/web_01/src/dictionary/Dictionary.java
@@ -1,5 +1,39 @@
 package dictionary;
 
-public class Dictionary {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
+public class Dictionary {
+	private List<DictionaryEntry> entries;
+
+	public Dictionary(List<DictionaryEntry> entries) {
+		entries.sort(null); // lexicographically sort
+		this.entries = entries;
+	}
+	
+	// return N most frequent words with given prefix
+	public List<String> getWordsWithPrefix(String prefix, int num) {
+		List<DictionaryEntry> prefixMatches = new ArrayList<DictionaryEntry>();
+		for (DictionaryEntry entry : this.entries) {
+			if (entry.word.startsWith(prefix)){
+				prefixMatches.add(entry);
+			}
+		}
+		// This time, sort by frequency
+		prefixMatches.sort(new Comparator<DictionaryEntry> () {
+			@Override
+			public int compare(DictionaryEntry entry1, DictionaryEntry entry2) {
+				return entry1.frequency - entry2.frequency;
+			}
+		});
+		List<DictionaryEntry> mostFrequent = prefixMatches.subList(0, num);
+		// Sort again to make result readable
+		mostFrequent.sort(null);
+		List<String> result = new ArrayList<String>();
+		for (DictionaryEntry entry : mostFrequent) {
+			result.add(entry.word);
+		}
+		return result;
+	}
 }
--- java/web/web_01/src/dictionary/DictionaryEntry.java
+++ java/web/web_01/src/dictionary/DictionaryEntry.java
@@ -1,10 +1,17 @@
 package dictionary;
 
-public class DictionaryEntry {
-	private String word;
-	private int frequency; // n'th most frequent word
+public class DictionaryEntry implements Comparable<DictionaryEntry> {
+	public String word;
+	public int frequency; // n'th most frequent word
+
 	public DictionaryEntry(String word, int frequency) {
-		word = word;
-		frequency = frequency;
+		this.word = word;
+		this.frequency = frequency;
 	}
+
+	@Override
+	public int compareTo(DictionaryEntry entry) {
+		return word.compareTo(entry.word);
+	}
+
 }
