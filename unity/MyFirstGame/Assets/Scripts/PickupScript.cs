247aaaf795af87b1d9eb143722a17563bb537a1c
Calvin Caulfield <calvin.caulfield@gmail.com>
2015-08-16 18:53:34 +0900

    Completed first example project.

---
+++ unity/MyFirstGame/Assets/Scripts/PickupScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupScript : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, 1, 0) * Time.deltaTime * 100, Space.World);
+	}
+}
--- unity/MyFirstGame/Assets/Scripts/PlayerScript.cs
+++ unity/MyFirstGame/Assets/Scripts/PlayerScript.cs
@@ -1,21 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour {
 	
 	private Rigidbody rb;
 	public float speed;
 
+	private int count;
+	public Text countText;
+	public Text winText;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		refreshCountText ();
+		winText.text = "";
 	}
 	
-	
+	void refreshCountText() {
+		countText.text = "Count: " + count.ToString ();
+	}
+
 	void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 		
 		rb.AddForce (new Vector3(moveHorizontal, 0.0f, moveVertical ) * speed);
+	}
+
+	// Destroy everything that enters the trigger
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pickup")) {
+			other.gameObject.SetActive(false);
+			count = count + 1;
+			refreshCountText();
+			if (count == 2) {
+				winText.text = "YOU WON!";
+
+			}
+		}
 	}
 }
