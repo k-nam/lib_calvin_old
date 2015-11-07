d89cb2c2c1a41fff6e6e8c5ab774b4beb6ca065b
Calvin Caulfield <calvin.caulfield@gmail.com>
2015-11-07 15:11:00 +0900

    Started VB

---
+++ vb/practice/WindowsApplication1/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        AddHandler Button1.Click, AddressOf Me.Button_Click
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As EventArgs)
+        MsgBox("Clicked!", , )
+    End Sub
+End Class
