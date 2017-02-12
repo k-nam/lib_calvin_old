2b57199e767fded6447acae0c915b8951a59e449
Calvin Caulfield <calvin.caulfield@gmail.com>
2017-02-12 17:30:41 +0900

    Added performance test for non-square matrices. It turned out that MKL DGEMM is slow for matrix*vector for vector*vector operations. DGEMM seems to be optimized for square matrices.

--- cpp/lib_calvin/matrix/matrix_test.cc
+++ cpp/lib_calvin/matrix/matrix_test.cc
@@ -5,16 +5,21 @@
 #endif
 
 
-//#include "mkl/include/mkl_boost_ublas_matrix_prod.hpp" // prod using MKL
-#include "boost/numeric/ublas/matrix.hpp" // prod using BOOST implementation
+#include "mkl/include/mkl_boost_ublas_matrix_prod.hpp" // prod using MKL
+//#include "boost/numeric/ublas/matrix.hpp" // prod using BOOST implementation
 
 void lib_calvin_matrix::matrixTest() {
-	size_t testSize = 1000;
-	lib_calvin_matrix::mklTest(testSize);
-	std::cout << "---------- Beginning matrix test -----------\n\n";	
 	using boost::numeric::ublas::matrix;
 	lib_calvin::stopwatch watch;
 	typedef double NumericType;
+	size_t testSize = 1000;
+	lib_calvin_matrix::mklTest(testSize);
+
+	//lib_calvin::matrix<NumericType> t1(10000);
+	//lib_calvin::matrix<NumericType> t2(10000);
+	//auto t3 = t1 * t2;
+
+	std::cout << "---------- Beginning matrix test -----------\n\n";	
 	watch.start();
 	double rtv = doGigaOps();
 	watch.stop();
@@ -34,7 +39,7 @@ void lib_calvin_matrix::matrixTest() {
 
 double lib_calvin_matrix::doGigaOps() {
 	int const giga = 1000000000;
-	int const arraySize = 1000;
+	int const arraySize = 300;
 	int const iter = giga / arraySize;
 	double *a = new double[arraySize];
 	double *b = new double[arraySize];
@@ -67,48 +72,62 @@ void lib_calvin_matrix::mklTest(size_t size) {
 	using boost::numeric::ublas::matrix;
 	lib_calvin::stopwatch watch;
 
+	size_t const size2 = 100;
+
 	matrix<NumericType> a(size, size);
 	matrix<NumericType> b(size, size);
+	matrix<NumericType> c(size2, size);
 	matrix<NumericType> x(size, 1);
+	matrix<NumericType> y(1, size);
 	for (int i = 0; i < size; ++i) {
 		for (int j = 0; j < size; j++) {
 			a(i, j) = 1.2;
 			b(i, j) = 1.5;
 		}
 		x(i, 0) = 1.5;
+		y(0, i) = 1.2;
 	}
 
 	watch.start();
-	matrix<NumericType> c = prod(a, b);
+	matrix<NumericType> result = prod(a, b);
 	watch.stop();
 	std::cout << "mkl size: " << size << ". " << watch.read() << "  GFLOPS: " <<
-		(double)size*size*size * 2 / watch.read() / 1000000000 << "\n";
+		(double)size * size * size * 2 / watch.read() / 1000000000 << "\n";
 	for (int i = 0; i < size; ++i) {
 		for (int j = 0; j < size; j++) {
-			double residual = c(i, j) - 1.2 * 1.5 * size;
+			double residual = result(i, j) - 1.2 * 1.5 * size;
 			if (residual*residual > 0.01) {
 				std::cout << "MKL is lie\n";
 				exit(0);
 			}
 		}
 	}
-	std::cout << "\n";
-
 	watch.start();
-	c = prod(a, x);
+	result = prod(c, x);
 	watch.stop();
 	std::cout << "mkl matrix*vector: " << size << ". " << watch.read() << "  GFLOPS: " <<
-		(double)size*size * 2 / watch.read() / 1000000000 << "\n";
-
-
-	lib_calvin::matrix<NumericType> e1(size, size);
-	lib_calvin::matrix<NumericType> f(size, 1);
+		(double)size * size2 * 2 / watch.read() / 1000000000 << "\n";
 	watch.start();
-	auto g = e1 * f;
+	result = prod(y, x);
+	watch.stop();
+	std::cout << "mkl vector*vector: " << size << ". " << watch.read() << "  GFLOPS: " <<
+		(double)size * 2 / watch.read() / 1000000000 << "\n";
+	std::cout << "\n";
+
+	lib_calvin::matrix<NumericType> c1(size2, size);
+	lib_calvin::matrix<NumericType> x1(size, 1);
+	lib_calvin::matrix<NumericType> y1(1, size);
+	watch.start();
+	auto result1 = c1 * x1;
 	watch.stop();
 	std::cout << "lib_calvin matrix*vector: " << size << ". " << watch.read() << "  GFLOPS: " <<
-		(double)size*size * 2 / watch.read() / 1000000000 << "\n";
-
+		(double)size * size2 * 2 / watch.read() / 1000000000 << "\n";
+	watch.start();
+	result1.reset(y1 * x1);
+	watch.stop();
+	std::cout << "lib_calvin vector*vector: " << size << ". " << watch.read() << "  GFLOPS: " <<
+		(double)size * 2 / watch.read() / 1000000000 << "\n";
 	std::cout << "\n";
+
 	std::cout << "------------- mklTest finished ---------------\n\n\n";
 }
