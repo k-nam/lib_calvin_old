258023453533fd1b7d9156ea085f3d980f637ee8
Calvin Caulfield <calvin.caulfield@gmail.com>
2017-02-19 21:04:49 +0900

    Finished basic test routine. Performance is about 10GFLOPS.

--- cpp/lib_calvin/matrix/matrix_multi.asm
+++ cpp/lib_calvin/matrix/matrix_multi.asm
@@ -3,13 +3,58 @@
 ExitProcess proto
 .data
 sum qword 0
-.code
-doAssemblyGigaOp proc
-   mov  rax,5
-   add  rax,6
-   mov  sum,rax
 
-   mov  ecx,0
-   ret
-doAssemblyGigaOp endp
-end
+
+.code
+doAssemblyGigaOp PROC
+	; params:
+	; rcx = src
+	; rdx = target
+	; r8 = size
+	; r9 = # iter
+
+	; locals:
+	; r10 = index1 ( r10 < size, innerloop)
+	; r11 = index2 ( r11 < #iter, outerloop)
+
+	; vars:
+	; ymm1: multiplier
+	; 
+
+	sub rsp, 40h
+
+	vzeroall
+	mov qword ptr[rsp + 8h], r12 ; push
+	
+	;mov qword ptr[rsp], rcx
+	;mov rax, rsp
+	vbroadcastsd ymm1, qword ptr[rdx]	
+
+	mov	r11, 0
+outerloop:	
+	cmp r11, r9
+	jge outerloopend
+	mov	r10, 0
+innerloop:
+	cmp r10, r8
+	jge innerloopend
+	imul r12, r10, 8
+	vmovapd ymm2, ymmword ptr[rcx + r12]
+	vmovapd ymm3, ymmword ptr[rcx + r12 + 32]
+	vmulpd ymm2, ymm2, ymm1
+	vmulpd ymm3, ymm3, ymm1
+	vmovapd ymmword ptr[rdx + r12], ymm2
+	vmovapd ymmword ptr[rdx + r12 + 32], ymm3
+	add r10, 8 ; ymm register contain 4 double values, using two ymm registers
+	jmp innerloop
+innerloopend:
+	add r11, 1
+	jmp outerloop
+outerloopend:	
+
+	mov rax, qword ptr[rsp]
+	mov r12, qword ptr[rsp + 8h] ; pop
+	add rsp, 40h		
+	ret
+doAssemblyGigaOp ENDP
+End
