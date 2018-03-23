7477dd83317d0bf8a8370b8f2967559bf5d41df4
Calvin Caulfield <calvin.caulfield@gmail.com>
2018-03-23 22:07:49 +0900

    no message

--- lib_calvin/graph/graph.h
+++ lib_calvin/graph/graph.h
@@ -1487,7 +1487,7 @@ void kruskal(vector<vector<pair<size_t, W>>> const &graph,
 	numE = static_cast<size_t>(edges.size());
 	curE = 0;
 	// Sorting
-	lib_calvin::introSort(edges.begin(), edges.end());
+	lib_calvin::sort(edges.begin(), edges.end());
 	// Growing MST
 	for (size_t i = 0; i < numE; ++i) {
 		// Insert edge if it connects two sets
