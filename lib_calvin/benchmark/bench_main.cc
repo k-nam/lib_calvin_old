85c91529760758c949ef6e7008cf4e9e79c18fd2
Calvin Caulfield <calvin.caulfield@gmail.com>
2018-03-29 17:07:02 +0900

    no message

--- lib_calvin/benchmark/bench_main.cc
+++ lib_calvin/benchmark/bench_main.cc
@@ -1,9 +1,13 @@
 #include "sorting_bench.h"
 #include "container_bench.h"
+#include "matrix_bench.h"
+
 
 int main(int argc, char* argv[])
 {
 	//lib_calvin_benchmark::sorting::sortBench();
-	lib_calvin_benchmark::container::containerBench();
+	//lib_calvin_benchmark::container::containerBench();
+	lib_calvin_benchmark::matrix::matrixBench();
+
 	return 0;
 }
