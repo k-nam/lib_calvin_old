4bf8a506c78a55de2d194ea818d83d47aa2a8696
Calvin Caulfield <calvin.caulfield@gmail.com>
2018-07-03 16:47:02 +0900

    no message

--- lib_calvin/benchmark/bench_main.cc
+++ lib_calvin/benchmark/bench_main.cc
@@ -10,8 +10,8 @@ int main(int argc, char* argv[])
 	//lib_calvin_benchmark::sorting::sortBench();
 	//lib_calvin_benchmark::container::containerBench();
 	//lib_calvin_benchmark::matrix::matrixBench();
-	lib_calvin_benchmark::string::stringBench();
+	//lib_calvin_benchmark::string::stringBench();
 	//lib_calvin_benchmark::graph::graphBench();
-	//lib_calvin_benchmark::etc::etc_bench();
+	lib_calvin_benchmark::etc::etc_bench();
 	return 0;
 }
